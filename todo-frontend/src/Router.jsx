2afdf9b51063484f2919db8bd65519f43eb9e6f0
けん <keiju.love@gmail.com>
2021-06-07 23:50:19 +0900

    ルーティングファイル作成

---
+++ todo-frontend/src/Router.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Top from '../components/Top';
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+        </BrowserRouter>
+    );
+};
+export default Router;
