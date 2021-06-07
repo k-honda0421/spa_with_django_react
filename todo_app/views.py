3af664b5c8f6926307f032d83cc7c61614fca8f3
けん <keiju.love@gmail.com>
2021-06-07 22:02:47 +0900

    Todoのviewを新規作成

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import filters, generics, viewsets
 
-# Create your views here.
+from .models import Todo
+from .serializer import TodoSerializer
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    fileter_fields = ("name",)
